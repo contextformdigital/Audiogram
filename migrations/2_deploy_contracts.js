ccd7a6766ecbb8fdb5769611b0552aff9c9776e7
Casey O'Neill <caseyoneill78@hotmail.com>
2018-11-16 09:56:30 -0800

    moved placement of variables in an attempt to clean up migrations file.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
 const Audiogram = artifacts.require('Audiogram');
 
-let name = 'Audiogram';
-let symbol = 'AUDIO';
-
 module.exports = ((deployer) => {
-	deployer.deploy(Audiogram, name, symbol);
+	deployer.deploy(Audiogram, 'Audiogram', 'AUDIO');
 })
